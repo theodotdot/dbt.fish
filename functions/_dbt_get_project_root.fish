a2b3793d012496b306dfd2060e1d71ffe64e4cec
theodotdot <59372255+theodotdot@users.noreply.github.com>
2025-11-03 20:15:03 +0100

    Use DBT_PROJECT_DIR when available

--- functions/_dbt_get_project_root.fish
+++ functions/_dbt_get_project_root.fish
@@ -1,4 +1,12 @@
 function _dbt_get_project_root --description "Find dbt project root directory"
+    # Check if DBT_PROJECT_DIR is set and valid
+    if set -q DBT_PROJECT_DIR
+        and test -f "$DBT_PROJECT_DIR/dbt_project.yml"
+        echo $DBT_PROJECT_DIR
+        return 0
+    end
+
+    # Fall back to searching from current directory
     set -l current_dir (pwd)
     while test "$current_dir" != /
         if test -f "$current_dir/dbt_project.yml"
