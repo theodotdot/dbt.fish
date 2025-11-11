6a0b85b8f8c2f5014666ce1bde75652362153a1a
theodotdot <59372255+theodotdot@users.noreply.github.com>
2025-11-11 10:21:26 +0100

    Disabling low value extractors for performance
    Commented out several extraction functions to improve performance.

--- functions/_dbt_get_all_selectors.fish
+++ functions/_dbt_get_all_selectors.fish
@@ -3,10 +3,13 @@ function _dbt_get_all_selectors --description "Get all available selectors with 
 
     # Combine all selector types
     _dbt_extract_models $prefix
-    _dbt_extract_tags $prefix
-    _dbt_extract_sources $prefix
-    _dbt_extract_exposures $prefix
-    _dbt_extract_metrics $prefix
     _dbt_extract_fqns $prefix
-    _dbt_extract_hardcoded $prefix
+
+    # No big value for those atm and takes a toll on perf when enabled
+    #_dbt_extract_hardcoded $prefix
+    #_dbt_extract_tags $prefix
+    #_dbt_extract_sources $prefix
+    #_dbt_extract_exposures $prefix
+    #_dbt_extract_metrics $prefix
+
 end
